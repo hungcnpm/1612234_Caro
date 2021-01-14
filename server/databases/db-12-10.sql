4ed04fd14ec1611046422f17ce1a464cd870fcf6
hungcnpm <45176783+hungcnpm@users.noreply.github.com>
2021-01-14 20:38:54 +0700

    Update db-12-10.sql

--- server/databases/db-12-10.sql
+++ server/databases/db-12-10.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `web-final-1612234` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `web-final-1612234`;
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
--- Host: localhost    Database: web-btcn06-1612422
+-- Host: 127.0.0.1    Database: web-final-1612234
 -- ------------------------------------------------------
--- Server version	8.0.17
+-- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,18 +27,12 @@ CREATE TABLE `users` (
   `email` varchar(45) DEFAULT NULL,
   `password` varchar(245) DEFAULT NULL,
   `fullname` varchar(45) DEFAULT NULL,
+  `WinCount` int DEFAULT '0',
+  `DrawCount` int DEFAULT '0',
+  `LoseCount` int DEFAULT '0',
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +43,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-12 13:25:00
+-- Dump completed on 2021-01-11  9:10:23
